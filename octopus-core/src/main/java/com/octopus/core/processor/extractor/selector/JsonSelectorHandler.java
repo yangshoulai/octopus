7ef634521469c62766ee1c14c4ecff5c90a12cc1
yangshoulai <shoulai.yang@gmail.com>
2023-12-25 09:29:47 +0800

    fix bug

--- octopus-core/src/main/java/com/octopus/core/processor/extractor/selector/JsonSelectorHandler.java
+++ octopus-core/src/main/java/com/octopus/core/processor/extractor/selector/JsonSelectorHandler.java
@@ -5,9 +5,11 @@ import com.jayway.jsonpath.Configuration;
 import com.jayway.jsonpath.JsonPath;
 import com.jayway.jsonpath.Option;
 import com.octopus.core.Response;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+
 import net.minidev.json.JSONArray;
 
 /**
@@ -16,32 +18,34 @@ import net.minidev.json.JSONArray;
  */
 public class JsonSelectorHandler extends CacheableSelectorHandler<String> {
 
-  private static final Configuration CONFIGURATION =
-      Configuration.builder()
-          .options(Option.ALWAYS_RETURN_LIST)
-          .options(Option.SUPPRESS_EXCEPTIONS)
-          .build();
+    private static final Configuration CONFIGURATION =
+            Configuration.builder()
+                    .options(Option.ALWAYS_RETURN_LIST)
+                    .options(Option.SUPPRESS_EXCEPTIONS)
+                    .build();
 
-  @Override
-  public List<String> doSelectWithDoc(
-      String json, Selector selector, boolean multi, Response response) {
-    List<String> list = new ArrayList<>();
-    Object obj = JsonPath.using(CONFIGURATION).parse(json).read(selector.value());
-    if (obj instanceof JSONArray) {
-      JSONArray array = (JSONArray) obj;
-      for (Object o : array) {
-        if (o instanceof Map || o instanceof List) {
-          list.add(JSONUtil.toJsonStr(o));
-        } else {
-          list.add(o.toString());
+    @Override
+    public List<String> doSelectWithDoc(
+            String json, Selector selector, boolean multi, Response response) {
+        List<String> list = new ArrayList<>();
+        Object obj = JsonPath.using(CONFIGURATION).parse(json).read(selector.value());
+        if (obj instanceof JSONArray) {
+            JSONArray array = (JSONArray) obj;
+            for (Object o : array) {
+                if (o != null) {
+                    if (o instanceof Map || o instanceof List) {
+                        list.add(JSONUtil.toJsonStr(o));
+                    } else {
+                        list.add(o.toString());
+                    }
+                }
+            }
         }
-      }
+        return list;
     }
-    return list;
-  }
 
-  @Override
-  protected String parse(String content) {
-    return content;
-  }
+    @Override
+    protected String parse(String content) {
+        return content;
+    }
 }
