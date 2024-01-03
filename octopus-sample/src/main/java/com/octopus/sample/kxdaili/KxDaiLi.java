2dbade730a7cbee4b1040e69ebbab8da6b8f8ab3
yangshoulai <shoulai.yang@gmail.com>
2024-01-03 10:57:09 +0800

    Fix KxDaiLi

--- octopus-sample/src/main/java/com/octopus/sample/kxdaili/KxDaiLi.java
+++ octopus-sample/src/main/java/com/octopus/sample/kxdaili/KxDaiLi.java
@@ -10,7 +10,9 @@ import com.octopus.core.processor.extractor.selector.Css;
 import com.octopus.core.processor.extractor.selector.Formatter;
 import com.octopus.core.processor.extractor.selector.Xpath;
 import com.octopus.core.processor.matcher.Matchers;
+
 import java.util.List;
+
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
@@ -23,60 +25,62 @@ import lombok.extern.slf4j.Slf4j;
 @Extractor
 public class KxDaiLi {
 
-  @Css(expression = "table.active tbody tr", self = true)
-  private List<KxDaiLiProxy> proxies;
+    @Css(expression = "table.active tbody tr", self = true)
+    private List<KxDaiLiProxy> proxies;
 
-  @LinkMethod
-  public String nextPage(Response response) {
-    if (this.proxies != null && !this.proxies.isEmpty()) {
-      String url = response.getRequest().getUrl();
-      int page = Integer.parseInt(ReUtil.get(".*/dailiip/1/(\\d+)\\.html$", url, 1));
-      return "/dailiip/1/" + (++page) + ".html";
+    @LinkMethod
+    public String nextPage(Response response) {
+        if (this.proxies != null && !this.proxies.isEmpty()) {
+            String url = response.getRequest().getUrl();
+            int page = Integer.parseInt(ReUtil.get(".*/dailiip/1/(\\d+)\\.html$", url, 1));
+            return "/dailiip/1/" + (++page) + ".html";
+        }
+        return null;
     }
-    return null;
-  }
 
-  @Data
-  @Extractor
-  public static class KxDaiLiProxy {
+    @Data
+    @Extractor
+    public static class KxDaiLiProxy {
 
-    @Css("//td[1]/text()")
-    private String host;
+        @Xpath("//td[1]/text()")
+        private String host;
 
-    @Css("//td[2]/text()")
-    private int port;
+        @Xpath("//td[2]/text()")
+        private int port;
 
-    @Css("//td[3]/text()")
-    private String level;
+        @Xpath("//td[3]/text()")
+        private String level;
 
-    @Css(expression = "//td[4]/text()", formatter = @Formatter(split = true))
-    private String[] types;
+        @Xpath(expression = "//td[4]/text()", formatter = @Formatter(split = true))
+        private String[] types;
 
-    @Xpath(expression = "//td[5]/text()", formatter = @Formatter(regex = "^(.*) 秒$", groups = 1))
-    private double responseSeconds;
+        @Xpath(expression = "//td[5]/text()", formatter = @Formatter(regex = "^(.*) 秒$", groups = 1))
+        private double responseSeconds;
 
-    @Xpath(expression = "//td[6]/text()")
-    private String location;
+        @Xpath(expression = "//td[6]/text()")
+        private String location;
 
-    @Xpath(expression = "//td[7]/text()")
-    private String lastVerifyTime;
-  }
+        @Xpath(expression = "//td[7]/text()")
+        private String lastVerifyTime;
+    }
 
-  public static void main(String[] args) {
-    Octopus.builder()
-        .addProcessor(
-            Matchers.ALL,
-            KxDaiLi.class,
-            kxDaiLi -> {
-              kxDaiLi
-                  .getProxies()
-                  .forEach(
-                      p -> {
-                        log.debug("{}", JSONUtil.toJsonStr(p));
-                      });
-            })
-        .addSeeds("http://www.kxdaili.com/dailiip/1/1.html")
-        .build()
-        .start();
-  }
+    public static void main(String[] args) {
+        Octopus.builder()
+                .addProcessor(
+                        Matchers.ALL,
+                        KxDaiLi.class,
+                        kxDaiLi -> {
+                            if (kxDaiLi.getProxies() != null) {
+                                kxDaiLi
+                                        .getProxies()
+                                        .forEach(
+                                                p -> {
+                                                    log.debug("{}", JSONUtil.toJsonStr(p));
+                                                });
+                            }
+                        })
+                .addSeeds("http://www.kxdaili.com/dailiip/1/1.html")
+                .build()
+                .start();
+    }
 }
