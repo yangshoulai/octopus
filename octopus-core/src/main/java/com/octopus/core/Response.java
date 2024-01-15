18c3383ecb5374342ab554786cc7ac71feb18af1
yangshoulai <shoulai.yang@gmail.com>
2024-01-15 14:49:31 +0800

    添加注释

--- octopus-core/src/main/java/com/octopus/core/Response.java
+++ octopus-core/src/main/java/com/octopus/core/Response.java
@@ -3,13 +3,14 @@ package com.octopus.core;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.json.JSON;
 import cn.hutool.json.JSONUtil;
+import lombok.NonNull;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
 import java.io.Serializable;
 import java.nio.charset.Charset;
 import java.util.HashMap;
 import java.util.Map;
-import lombok.NonNull;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
 
 /**
  * @author shoulai.yang@gmail.com
@@ -17,102 +18,100 @@ import org.jsoup.nodes.Document;
  */
 public class Response implements Serializable {
 
-  private Request request;
+    private Request request;
 
-  private int status;
+    private int status;
 
-  private byte[] body = new byte[0];
+    private byte[] body = new byte[0];
 
-  private Map<String, String> headers = new HashMap<>();
+    private Map<String, String> headers = new HashMap<>();
 
-  private Charset charset;
+    private Charset charset;
 
-  private String mimeType;
+    private String mimeType;
 
-  private String text;
+    private String text;
 
-  private Document document;
-
-  public Response(@NonNull Request request) {
-    this.request = request;
-  }
-
-  public void setRequest(Request request) {
-    this.request = request;
-  }
-
-  public Request getRequest() {
-    return request;
-  }
-
-  public int getStatus() {
-    return status;
-  }
-
-  public Response setStatus(int status) {
-    this.status = status;
-    return this;
-  }
-
-  public byte[] getBody() {
-    return body;
-  }
-
-  public Response setBody(byte[] body) {
-    this.body = body;
-    return this;
-  }
-
-  public Map<String, String> getHeaders() {
-    return headers;
-  }
-
-  public Response setHeaders(@NonNull Map<String, String> headers) {
-    this.headers = headers;
-    return this;
-  }
-
-  public Charset getCharset() {
-    return charset;
-  }
-
-  public Response setCharset(Charset charset) {
-    this.charset = charset;
-    return this;
-  }
-
-  public String getMimeType() {
-    return mimeType;
-  }
-
-  public Response setMimeType(String mimeType) {
-    this.mimeType = mimeType;
-    return this;
-  }
-
-  public final boolean isSuccessful() {
-    return this.status >= 200 && this.status < 300;
-  }
-
-  public String asText() {
-    if (StrUtil.isBlank(text)) {
-      text = new String(this.body, this.charset);
+    public Response(@NonNull Request request) {
+        this.request = request;
     }
-    return text;
-  }
 
-  public Document asDocument() {
-    return Jsoup.parse(this.asText());
-  }
+    public void setRequest(Request request) {
+        this.request = request;
+    }
 
-  public JSON asJson() {
-    return JSONUtil.parse(this.asText());
-  }
+    public Request getRequest() {
+        return request;
+    }
 
-  @Override
-  public String toString() {
-    return String.format(
-        "Response [status = %s, request = %s, charset = %s, length = %s bytes]",
-        status, request, charset, body.length);
-  }
+    public int getStatus() {
+        return status;
+    }
+
+    public Response setStatus(int status) {
+        this.status = status;
+        return this;
+    }
+
+    public byte[] getBody() {
+        return body;
+    }
+
+    public Response setBody(byte[] body) {
+        this.body = body;
+        return this;
+    }
+
+    public Map<String, String> getHeaders() {
+        return headers;
+    }
+
+    public Response setHeaders(@NonNull Map<String, String> headers) {
+        this.headers = headers;
+        return this;
+    }
+
+    public Charset getCharset() {
+        return charset;
+    }
+
+    public Response setCharset(Charset charset) {
+        this.charset = charset;
+        return this;
+    }
+
+    public String getMimeType() {
+        return mimeType;
+    }
+
+    public Response setMimeType(String mimeType) {
+        this.mimeType = mimeType;
+        return this;
+    }
+
+    public final boolean isSuccessful() {
+        return this.status >= 200 && this.status < 300;
+    }
+
+    public String asText() {
+        if (StrUtil.isBlank(text)) {
+            text = new String(this.body, this.charset);
+        }
+        return text;
+    }
+
+    public Document asDocument() {
+        return Jsoup.parse(this.asText());
+    }
+
+    public JSON asJson() {
+        return JSONUtil.parse(this.asText());
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Response [status = %s, request = %s, charset = %s, length = %s bytes]",
+                status, request, charset, body.length);
+    }
 }
