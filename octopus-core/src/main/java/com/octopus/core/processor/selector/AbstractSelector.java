85511bb01fbc733ff47c2a1b285a1e6d5486e438
yangshoulai <shoulai.yang@gmail.com>
2024-01-23 09:07:12 +0800

    增加自定义收集器、存储器、处理器配置

--- octopus-core/src/main/java/com/octopus/core/processor/selector/AbstractSelector.java
+++ octopus-core/src/main/java/com/octopus/core/processor/selector/AbstractSelector.java
@@ -20,71 +20,73 @@ import java.util.stream.Collectors;
  * @date 2024/01/12
  */
 public abstract class AbstractSelector<P extends AbstractSelectorProperties> implements Selector<P> {
-  @Override
-  public List<String> select(String source, boolean multi, P selector, Response response)
-      throws SelectException {
-    try {
-      List<String> selected;
-      if (multi) {
-        selected = this.doMultiSelect(source, selector, response);
-      } else {
-        selected = ListUtil.of(this.doSingleSelect(source, selector, response));
-      }
-      if (selected == null) {
-        selected = ListUtil.of(selector.getDefaultValue());
-      }
-      selected = selected.stream().filter(Objects::nonNull).collect(Collectors.toList());
-      if (selector.getDenoiser() != null) {
-        selected = this.applyFormatter(selector.getDenoiser(), selected);
-      }
-      return multi || selected.isEmpty()
-          ? ListUtil.unmodifiable(selected)
-          : ListUtil.of(selected.get(0));
-    } catch (Exception e) {
-      throw new SelectException("error when select from " + response + " with " + selector, e);
+    @Override
+    public List<String> select(String source, boolean multi, P selector, Response response)
+            throws SelectException {
+        try {
+            List<String> selected;
+            if (multi) {
+                selected = this.doMultiSelect(source, selector, response);
+            } else {
+                selected = ListUtil.of(this.doSingleSelect(source, selector, response));
+            }
+            if (selected == null) {
+                selected = ListUtil.of(selector.getDefaultValue());
+            }
+            selected = selected.stream().filter(Objects::nonNull).collect(Collectors.toList());
+            if (selector.getDenoiser() != null) {
+                selected = this.applyDenoiser(selector.getDenoiser(), selected);
+            }
+            return multi || selected.isEmpty()
+                    ? ListUtil.unmodifiable(selected)
+                    : ListUtil.of(selected.get(0));
+        } catch (Exception e) {
+            throw new SelectException("error when select from " + response + " with " + selector, e);
+        }
     }
-  }
 
-  private List<String> applyFormatter(DenoiserProperties denoiser, List<String> selected) {
-    if (denoiser.isSplit()) {
-      selected =
-          selected.stream()
-              .flatMap(s -> Arrays.stream(s.split(denoiser.getSeparator())))
-              .collect(Collectors.toList());
+    private List<String> applyDenoiser(DenoiserProperties denoiser, List<String> selected) {
+        if (denoiser.isSplit()) {
+            selected =
+                    selected.stream()
+                            .flatMap(s -> Arrays.stream(s.split(denoiser.getSeparator())))
+                            .collect(Collectors.toList());
+        }
+        if (denoiser.isFilter()) {
+            selected = selected.stream().filter(StrUtil::isNotBlank).collect(Collectors.toList());
+        }
+        if (denoiser.isTrim()) {
+            selected = selected.stream().map(StrUtil::trim).collect(Collectors.toList());
+        }
+        if (StrUtil.isNotBlank(denoiser.getRegex())) {
+            selected =
+                    selected.stream()
+                            .map(
+                                    s -> {
+                                        int[] groups =
+                                                denoiser.getGroups() == null || denoiser.getGroups().length <= 0
+                                                        ? new int[]{0}
+                                                        : denoiser.getGroups();
+                                        List<String> args = new ArrayList<>();
+                                        for (int group : groups) {
+                                            String groupVal = ReUtil.get(denoiser.getRegex(), s, group);
+                                            args.add(groupVal == null ? "" : groupVal);
+                                        }
+                                        return String.format(denoiser.getFormat(), args.toArray());
+                                    })
+                            .collect(Collectors.toList());
+        } else if (StrUtil.isNotBlank(denoiser.getFormat())) {
+            selected = selected.stream().map(s -> String.format(denoiser.getFormat(), s)).collect(Collectors.toList());
+        }
+        return selected;
     }
-    if (denoiser.isFilter()) {
-      selected = selected.stream().filter(StrUtil::isNotBlank).collect(Collectors.toList());
-    }
-    if (denoiser.isTrim()) {
-      selected = selected.stream().map(StrUtil::trim).collect(Collectors.toList());
-    }
-    if (StrUtil.isNotBlank(denoiser.getRegex())) {
-      selected =
-          selected.stream()
-              .map(
-                  s -> {
-                    int[] groups =
-                        denoiser.getGroups() == null || denoiser.getGroups().length <= 0
-                            ? new int[] {0}
-                            : denoiser.getGroups();
-                    List<String> args = new ArrayList<>();
-                    for (int group : groups) {
-                      String groupVal = ReUtil.get(denoiser.getRegex(), s, group);
-                      args.add(groupVal == null ? "" : groupVal);
-                    }
-                    return String.format(denoiser.getFormat(), args.toArray());
-                  })
-              .collect(Collectors.toList());
-    }
-    return selected;
-  }
 
-  protected abstract List<String> doMultiSelect(
-          String source, P selector, Response response);
+    protected abstract List<String> doMultiSelect(
+            String source, P selector, Response response);
 
-  protected String doSingleSelect(String source, P selector, Response response)
-      throws SelectException {
-    List<String> result = this.doMultiSelect(source, selector, response);
-    return result == null || result.isEmpty() ? null : result.get(0);
-  }
+    protected String doSingleSelect(String source, P selector, Response response)
+            throws SelectException {
+        List<String> result = this.doMultiSelect(source, selector, response);
+        return result == null || result.isEmpty() ? null : result.get(0);
+    }
 }
