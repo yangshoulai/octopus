9461bb27616a3df95957fa9cdb8cc0febd43ac3a
yangshoulai <shoulai.yang@gmail.com>
2024-07-25 17:04:49 +0800

    更新执行器

--- octopus-executor/bin/octopus.sh
+++ octopus-executor/bin/octopus.sh
@@ -15,6 +15,13 @@ APP_HOME="$(
   pwd
 )"
 
+if [[ $# -le 0 || $# -gt 1 ]]; then
+  echo "Usage: $0 site.yaml"
+  exit 1
+fi
+
+site=$(basename "$1" | cut -d. -f1)
+
 CLASSPATH="$APP_HOME/conf"
 for i in "$APP_HOME"/lib/*.jar; do
    CLASSPATH="$CLASSPATH":"$i"
@@ -26,4 +33,4 @@ else
   JAVA="java"
 fi
 
-nohuo $JAVA -DAPP_HOME="$APP_HOME" $JVM_OPTS -classpath "$CLASSPATH" $MAIN_CLASS "$APP_HOME" "$@" > $APP_HOME/logs/octopus.log 2>&1 &
+nohup $JAVA -DAPP_HOME="$APP_HOME" $JVM_OPTS -classpath "$CLASSPATH" $MAIN_CLASS "$APP_HOME" "$1" > $APP_HOME/logs/$site.log 2>&1 &
--- octopus-executor/src/main/java/com/octopus/executor/App.java
+++ octopus-executor/src/main/java/com/octopus/executor/App.java
@@ -13,15 +13,11 @@ import java.util.Arrays;
 @Slf4j
 public class App {
     public static void main(String[] args) {
-        if (args.length != 2 || StrUtil.isBlank(args[0])) {
-            log.error("参数错误，请使用命令执行: ./octopus.sh site.yaml");
-        }
-        String[] sites = ArrayUtil.sub(args, 1, args.length);
-        sites = Arrays.stream(sites).filter(StrUtil::isNotBlank).toArray(String[]::new);
-        if (sites.length == 0) {
-            log.error("请提供配置文件");
-        }
-        for (String site : sites) {
+        try {
+            if (args.length != 2 || StrUtil.isBlank(args[0])) {
+                log.error("参数错误，请使用命令执行: ./octopus.sh site.yaml");
+            }
+            String site = args[1];
             site = site.endsWith(".yaml") ? site : site + ".yaml";
             File file = new File(site);
             if (!file.isAbsolute()) {
@@ -29,14 +25,12 @@ public class App {
             }
             if (!file.exists()) {
                 log.error("文件[%s]不存在");
-                break;
+                return;
             }
             log.info("execute octopus with conf file => {}", file.getPath());
-            try {
-                Octopus.fromYaml(file.getPath()).start();
-            } catch (Exception e) {
-                log.error("执行异常", e);
-            }
+            Octopus.fromYaml(file.getPath()).start();
+        } catch (Exception e) {
+            log.error("执行异常", e);
         }
     }
 }
